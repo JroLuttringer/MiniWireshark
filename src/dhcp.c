1b4cfdc516c5f2c62383155f965a8fc15171fc8a
jrluttringer <jrluttringer@etu.unistra.fr>
2018-01-05 15:46:07 +0100

    dhcp

--- src/dhcp.c
+++ src/dhcp.c
@@ -31,7 +31,11 @@ void process_bootp(const u_char* packet, int length){
     printf("\tServer Host Name: %s\n", bootp_info->bp_sname);
   else
     printf("\tServer Host Name Unknown\n");
-  printf("\tBoot File name: %s\n", bootp_info->bp_file;
+
+  if(bootp->bp_file)
+    printf("\tBoot File name: %s\n", bootp_info->bp_file;
+  else
+    printf("\tBoot File name unknown\n");
 
   int magic_cookie[4] = VM_RFC1048;
   int dhcp = 1;
@@ -51,26 +55,99 @@ void process_bootp(const u_char* packet, int length){
 
 
 
-  process_dhcp(const u_char* packet){
+void process_dhcp(const u_char* packet){
     int i = 0;
     int option;
-    printf("DHPC:\n");
+    printf("DHCP:\n");
     // vendor spec = 60 + 4 (magic cookie)
-    for(i=0; i < 60; i++){
+    while(i<60){
       //print Tlv
       int length = packet[i+1];
       if(packet[i] == TAG_END){
         printf(" End\n");
       }
-      print_dhcp_option(packet[i]);
-      i = i+2; // read T & L
-      // read V
-      while(length > 0){
-        printf("%01x ", packet[i++]);
-        length--;
+      print_dhcp_option(packet, i);
+
+      // TODO CHANGER
+      if(pakcet[i] == TAG_DHCP_MESSAGE){
+        i = i+3 // tout a déjà été lu dans les sous fonctions
+      } else {
+        i = i+2; // read T & L
+        // read V
+        while(length > 0){
+          printf("%01x ", packet[i++]);
+          length--;
+        }
       }
-      i-- ;
       printf("\n");
     }
 
+  }
+
+  void print_dhcp_option(const u_char* packet,int indice){
+      printf("\tOption: (%d) ", packet[indice]);
+      switch(packet[indice]){
+        case TAG_CLIENT_ID:
+          printf("Client identifier");
+        break;
+        case TAG_SUBNET_MASK:
+          printf("Subnet mask");
+        break;
+        case TAG_SERVER_ID:
+          printf("Server ID");
+        break;
+        case TAG_IP_LEASE:
+          printf("Ip lease time");
+        break;
+        case TAG_REBIND_TIME:
+          printf("Rebind time");
+        break;
+        case TAG_REQUESTED_IP:
+          printf("Requested IP:");
+        case TAG_PARM_REQUEST:
+          printf("Parameters request list");
+        break;
+        case TAG_RENEWAL_TIME:
+          printf("Renewal time");
+        break;
+        case TAG_DHCP_MESSAGE:
+          printf("DHCP Message type ");
+          print_dhcp_type(packet[indice+2]);
+        break;
+        default:
+          printf("Not supported");
+      }
+      printf("\n");
+  }
+
+  void print_dhcp_type(int type){
+    printf("Type: (%d) ",type);
+    switch(type){
+      case DHCPDISCOVER:
+        printf("(DISCOVER)");
+        break;
+      case DHCPOFFER:
+        printf("(OFFER)");
+      break;
+      case DHCPREQUEST:
+        printf("(REQUEST)");
+      break;
+      case DHCPDECLINE:
+        printf("(DECLINE)");
+      break;
+      case DHCPACK:
+        printf("(ACK)");
+      break;
+      case DHCPNACK:
+        printf("(NACK)");
+      break;
+      case DHCPRELEASE:
+        printf("(RELEASE)");
+      break;
+      case DHCPINFORM:
+        printf("(INFORM)");
+      break;
+
+
+    }
   }
